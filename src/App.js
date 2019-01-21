9fa321316546991f148a8ab93b3bc8cb12b96c8b
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-01-21 18:04:18 +0530

    Initaial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
+//eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()}
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header className="App-header">
+      Time is: {this.state.date.toLocaleTimeString()}
+      </header></div>
     );
   }
 }
