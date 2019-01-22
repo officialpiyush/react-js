e0272c2f34cbbe42f34a9ee29131b00420dcc5db
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-01-22 17:39:35 +0530

    ay ay ay

---
+++ src/components/landing.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'reacts';
+import '../static/index.css';
+
+class Landing extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            members: 0,
+            guilds: 0,
+            channels: 0
+        }
+    }
+
+    ComponentDidMount(){
+        
+    }
+}
+
+export default Landing;
